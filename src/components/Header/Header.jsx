c1db30590c6dcb8e47b13928458f600ea4f32634
NusratJahanGeek <nusrat.jahan.4338@gmail.com>
2023-04-11 09:18:24 +0600

    Header Finalized

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/details">Job Details</Link>
-            <Link to="/applied">Applied Jobs</Link>
+            <div className='top-bar'>
+                <Link to="/"><h2>JobSnap</h2></Link>
+                <div className='top-bar-link-items'>
+                    <Link to="/statistics">Statistics</Link>
+                    <Link to="/applied">Applied Jobs</Link>
+                    <Link to="/blog">Blog</Link>
+                </div>
+                <button className='apply-button'>Start Applying</button>
+            </div>
         </nav>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,8 @@ import Admin from './components/Layout/Admin';
 import Home from './components/Home/Home';
 import JobDetails from './components/JobDetails/JobDetails';
 import AppliedJobs from './components/AppliedJobs/AppliedJobs';
+import Statistics from './components/Statistics/Statistics';
+import Blog from './components/Blog/Blog';
 
 const router = createBrowserRouter([
   {
@@ -20,12 +22,16 @@ const router = createBrowserRouter([
         element: <Home></Home>
       },
       {
-        path: 'details',
-        element: <JobDetails></JobDetails>
+        path: 'statistics',
+        element: <Statistics></Statistics>
       },
       {
         path: 'applied',
         element: <AppliedJobs></AppliedJobs>
+      },
+      {
+        path: '/blog',
+        element: <Blog></Blog>
       }
     ]
   }
