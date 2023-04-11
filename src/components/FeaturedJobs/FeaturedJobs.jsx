cee39035d8b0a0629c27e3d2f26de65b4d4e2331
NusratJahanGeek <nusrat.jahan.4338@gmail.com>
2023-04-11 11:43:15 +0600

    Featured Section Added with All the Necessary Data

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import JobListing from '../JobListing/JobListing';
+import './FeaturedJobs.css'
 
 const FeaturedJobs = () => {
+    const joblistings = useLoaderData();
     return (
-        <div>
+        <div className='homepage-style'>
             <div className='section-title'>
-            <h2>Featured Jobs</h2>
-            <p>Explore thousands of job opportunities with all the information you need. Its your future.</p>
+                <h2>Featured Jobs</h2>
+                <p>Explore thousands of job opportunities with all the information you need. Its your future.</p>
             </div>
+
+            <div className="featured-jobs-container">
+            {
+                joblistings.map(joblisting => <JobListing
+                    key={joblisting.id}
+                    joblisting={joblisting}
+                ></JobListing>)
+            }
+            </div>
+            <button className='all-jobs-button'>See All Jobs</button>
         </div>
     );
 };
---
+++ src/components/JobListing/JobListing.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './JobListing.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faSackDollar } from '@fortawesome/free-solid-svg-icons';
+
+const JobListing = ({joblisting}) => {
+    const {id, companyLogo, companyName, jobTitle, companyLocation, features, salaryRange} = joblisting;
+    return (
+        <div className='joblisting-container'>
+            <img src={companyLogo} alt="" />
+            <h4>{jobTitle}</h4>
+            <p>{companyName}</p>
+            <p><small><span className='feature-style'>{features[0]}</span> <span className='feature-style'>{features[1]}</span></small></p>
+            <div className="location-salary-container">
+                <div className="location">
+                <p><FontAwesomeIcon icon={faLocationDot} /> {companyLocation}</p>
+           
+                </div>
+                <div className="salary">
+                <p><FontAwesomeIcon icon={faSackDollar} />  {salaryRange}</p>
+                </div>
+            </div>
+           <button className='apply-button'>View Details</button>
+        </div>
+    );
+};
+
+export default JobListing;
